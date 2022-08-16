224dca2b07bda4c9a43ff887aa8743e29483ba12
janbayram <honza.bayram@gmail.com>
2022-08-16 20:30:28 +0200

    added express

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var express = require('express');
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/plain" });
-    res.end("Hello world\n");
+var app = express();
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+    res.status(200).send("Je to tam.");
 });
 
-server.listen(3000, function() {
-    console.log('Server is running at 3000')
-});
+app.listen(PORT, function() {
+    console.log(`Server is running on port ${PORT}`);
+})
