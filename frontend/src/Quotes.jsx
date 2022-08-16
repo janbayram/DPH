fa8de0f04edc699844aab6381b007126a00b2048
janbayram <honza.bayram@gmail.com>
2022-08-16 21:03:07 +0200

    connecting react

--- frontend/src/Quotes.jsx
+++ frontend/src/Quotes.jsx
@@ -1,12 +1,25 @@
-import React, {useState} from "react";function Quotes() {
+import React, {useState} from "react";
+
+function Quotes() {
     const [text, setText] = useState("");
-    const [author, setAuthor] = useState("");return (
-      <div>
-        <button>
-          Generate Quote
-        </button>
-        <h1>{text}</h1>
-        <h3>{"-" + author}</h3>
-      </div>
-    )
-  }export default Quotes;
+    const [author, setAuthor] = useState("");
+
+
+    function getQuote() {
+        axios.get("http://localhost:8080/",  { crossdomain: true }).then(response => {
+            setText(response.data.text);
+            setAuthor(response.data.author);
+        });
+    }
+
+return (
+    <div>
+    <button>
+        Generate Quote
+    </button>
+    <h1>{text}</h1>
+    <h3>{"-" + author}</h3>
+    </div>
+)
+  }
+  export default Quotes;
